537a4ae67fe7c8912df5baf2a6d2fe4ecccd080e
azhar64888 <azharjamal648@gmail.com>
2025-10-27 09:49:53 +0500

    Task 1

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Azhar Jamal\n";
+    cout << "Registration No: 23089\n";
+    cout << "Course: Software Construction and Development\n";
+    return 0;
+}
